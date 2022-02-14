4dd0b06c34e0f1eba81db108c38bbe71e9d09e27
Aditya Neve <aditya.neve@gmail.com>
2022-02-15 00:07:03 +0530

    not working

---
+++ DSA/flower-management.js
@@ -0,0 +1,67 @@
+function flowerManagement(arr, K) {
+    // console.log('arr:', arr)
+    let count = K;
+    let index = 0;
+    while (index < arr.length) {
+        if (index === 0) {
+            if (arr[index + 1] === 0 && arr[index] !== 1) {
+                // arr[index] = 9;
+                count--;
+            }
+        }
+        if (index === arr.length - 1) {
+            if (arr[index - 1] === 0 && arr[index - 2] === 1 && arr[index] !== 1) {
+                // arr[index] = 9;
+                count--;
+            }
+        } else {
+            if (
+                arr[index + 1] === 0 &&
+                arr[index - 1] === 0 &&
+                arr[index] !== 1
+            ) {
+                // arr[index] = 9;
+                count--;
+            }
+        }
+        index++;
+    }
+    return count;
+}
+
+function runProgram(input) {
+    input = input.trim().split('\n');
+    let lines = 0;
+    for (let i = 0; i < +input[0]; i++) {
+        let [size, K] = input[++lines].trim().split(' ').map(Number);
+        let arr = input[++lines].trim().split(' ').map(Number);
+        let ans = flowerManagement(arr, K);
+        if (ans) {
+            console.log('No');
+        } else {
+            console.log('Yes');
+        }
+    }
+}
+if (process.env.USERNAME === 'aneve') {
+    runProgram(`1
+    6 2
+1 0 0 0 1 1`);
+} else {
+    process.stdin.resume();
+    process.stdin.setEncoding('ascii');
+    let read = '';
+    process.stdin.on('data', function (input) {
+        read += input;
+    });
+    process.stdin.on('end', function () {
+        read = read.replace(/\n$/, '');
+        read = read.replace(/\n$/, '');
+        runProgram(read);
+    });
+    process.on('SIGINT', function () {
+        read = read.replace(/\n$/, '');
+        runProgram(read);
+        process.exit(0);
+    });
+}
