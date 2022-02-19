7bbc4752b1c68471874d40a8b086d73289a3a0d5
Aditya Neve <aditya.neve@gmail.com>
2022-02-19 19:18:07 +0530

    ports updated

--- apartment-flats-manager/backend/src/server.js
+++ apartment-flats-manager/backend/src/server.js
@@ -2,10 +2,12 @@ const express = require('express');
 const connect = require('./configs/db');
 const app = express();
 
+const PORT = process.env.PORT || 5000;
+
 const start = async () => {
     await connect();
-    app.listen(3001, () => {
-        console.log('LISTENING ON SERVER 3001');
+    app.listen(PORT, () => {
+        console.log(`LISTENING ON SERVER ${PORT}`);
     });
 };
 
