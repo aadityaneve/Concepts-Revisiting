31ea468a92db03eb968ed9d37dcd81b0da8051e4
Aditya Neve <aditya.neve@gmail.com>
2022-02-23 00:00:07 +0530

    used: stack, logic: nearest greater element

---
+++ DSA/next-greater-element.js
@@ -0,0 +1,51 @@
+function nextGreaterElement(arr) {
+    let stack = [];
+    let newArr = [];
+
+    for (let i = arr.length - 1; i >= 0; i--) {
+        while (stack.length > 0 && stack[stack.length - 1] <= arr[i]) {
+            stack.pop();
+        }
+
+        if (stack.length > 0) {
+            newArr[i] = stack[stack.length - 1];
+        } else {
+            newArr[i] = -1;
+        }
+
+        stack[stack.length] = arr[i];
+    }
+    console.log(newArr.join(' '));
+}
+
+function runProgram(input) {
+    input = input.trim().split('\n');
+    let lines = 0;
+    for (let i = 0; i < +input[0]; i++) {
+        let size = +input[++lines];
+        let arr = input[++lines].trim().split(' ').map(Number);
+        nextGreaterElement(arr);
+    }
+}
+if (process.env.USERNAME === 'aneve') {
+    runProgram(`1
+  4
+  1 3 2 4`);
+} else {
+    process.stdin.resume();
+    process.stdin.setEncoding('ascii');
+    let read = '';
+    process.stdin.on('data', function (input) {
+        read += input;
+    });
+    process.stdin.on('end', function () {
+        read = read.replace(/\n$/, '');
+        read = read.replace(/\n$/, '');
+        runProgram(read);
+    });
+    process.on('SIGINT', function () {
+        read = read.replace(/\n$/, '');
+        runProgram(read);
+        process.exit(0);
+    });
+}
