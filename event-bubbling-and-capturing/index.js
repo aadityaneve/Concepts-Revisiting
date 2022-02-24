e252f7a1bf7a9d9e66e2d83b028e50cb2b82c0f2
Aditya Neve <aditya.neve@gmail.com>
2022-02-24 23:39:18 +0530

    event bubbling and capturing

---
+++ event-bubbling-and-capturing/index.js
@@ -0,0 +1,24 @@
+/* document.getElementById('grandparent').addEventListener('click', () => {
+    console.log('Grandparent Clicked!');
+});
+
+document.getElementById('parent').addEventListener('click', () => {
+    console.log('Parent Clicked!');
+});
+
+document.getElementById('child').addEventListener('click', () => {
+    console.log('Child Clicked!');
+});
+ */
+
+document.querySelector('#grandparent').addEventListener('click', () => {
+    console.log('Grandparent Clicked!');
+});
+
+document.querySelector('#parent').addEventListener('click', () => {
+    console.log('Parent Clicked!');
+});
+
+document.querySelector('#child').addEventListener('click', () => {
+    console.log('Child Clicked!');
+});
