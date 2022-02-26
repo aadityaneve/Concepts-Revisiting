2bf02e4a4879e7c08e26e672dc1ee758708d8552
Aditya Neve <aditya.neve@gmail.com>
2022-02-27 00:56:05 +0530

    Authentication and Protected Routes

--- react-router-dom/src/App.js
+++ react-router-dom/src/App.js
@@ -11,35 +11,50 @@ import { New } from './components/New';
 import { Users } from './components/Users';
 import { UserDetails } from './components/UserDetails';
 import { Admin } from './components/Admin';
+import { Profile } from './components/Profile';
+import { AuthProvider } from './components/auth';
+import { Login } from './components/Login';
+import { RequireAuth } from './components/RequireAuth';
 const LazyAbout = React.lazy(() => import('./components/About'));
 
 function App() {
     return (
         <div className='App'>
-            <Navbar />
-            <Routes>
-                <Route path='/' element={<Home />} />
-                {/* <Route path='about' element={<About />} /> */}
-                <Route
-                    path='about'
-                    element={
-                        <React.Suspense fallback='Loading.....'>
-                            <LazyAbout />
-                        </React.Suspense>
-                    }
-                />
-                <Route path='order-summary' element={<OrderSummary />} />
-                <Route path='products' element={<Products />}>
-                    <Route index element={<Featured />} />
-                    <Route path='featured' element={<Featured />} />
-                    <Route path='new' element={<New />} />
-                </Route>
-                <Route path='users' element={<Users />}>
-                    <Route path=':userId' element={<UserDetails />} />
-                    <Route path='admin' element={<Admin />} />
-                </Route>
-                <Route path='*' element={<NoMatch />} />
-            </Routes>
+            <AuthProvider>
+                <Navbar />
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                    {/* <Route path='about' element={<About />} /> */}
+                    <Route
+                        path='about'
+                        element={
+                            <React.Suspense fallback='Loading.....'>
+                                <LazyAbout />
+                            </React.Suspense>
+                        }
+                    />
+                    <Route path='order-summary' element={<OrderSummary />} />
+                    <Route path='products' element={<Products />}>
+                        <Route index element={<Featured />} />
+                        <Route path='featured' element={<Featured />} />
+                        <Route path='new' element={<New />} />
+                    </Route>
+                    <Route path='users' element={<Users />}>
+                        <Route path=':userId' element={<UserDetails />} />
+                        <Route path='admin' element={<Admin />} />
+                    </Route>
+                    <Route
+                        path='profile'
+                        element={
+                            <RequireAuth>
+                                <Profile />
+                            </RequireAuth>
+                        }
+                    />
+                    <Route path='login' element={<Login />} />
+                    <Route path='*' element={<NoMatch />} />
+                </Routes>
+            </AuthProvider>
         </div>
     );
 }
--- react-router-dom/src/components/Navbar.jsx
+++ react-router-dom/src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link, NavLink } from 'react-router-dom';
+import { useAuth } from './auth';
 
 export const Navbar = () => {
     const navLinkStyles = ({ isActive }) => {
@@ -8,6 +9,8 @@ export const Navbar = () => {
             textDecoration: isActive ? 'none' : 'underline',
         };
     };
+
+    const auth = useAuth();
 
     return (
         <nav>
@@ -20,6 +23,14 @@ export const Navbar = () => {
             <NavLink style={navLinkStyles} to='/products'>
                 Products
             </NavLink>
+            <NavLink style={navLinkStyles} to='/profile'>
+                Profile
+            </NavLink>
+            {!auth.user && (
+                <NavLink style={navLinkStyles} to='/login'>
+                    Login
+                </NavLink>
+            )}
         </nav>
     );
 };
