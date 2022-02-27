e0105dc51922f4208d0d03bfa8f02c8da2d93284
Aditya Neve <aditya.neve@gmail.com>
2022-02-27 16:02:15 +0530

    debouncing

--- JavaScript/debouncing/index.js
+++ JavaScript/debouncing/index.js
@@ -1,4 +1,4 @@
-let counter = 0;
+/* let counter = 0;
 const getData = () => {
     console.log('Fetching Data...', counter++);
 };
@@ -12,6 +12,26 @@ const debounce = (main, delay) => {
     timer = setTimeout(() => {
         main();
     }, delay);
-};
+}; */
 
 // debounce(getData, 1000);
+
+let counter = 0;
+const getData = () => {
+    console.log('Fetching Data....', counter++);
+};
+
+const debounce = function (fn, delay) {
+    let timer;
+    return function () {
+        let context = this;
+        let args = arguments;
+
+        clearInterval(timer);
+        timer = setTimeout(() => {
+            getData.apply(context, arguments);
+        }, delay);
+    };
+};
+
+const betterFunction = debounce(getData, 1000);
