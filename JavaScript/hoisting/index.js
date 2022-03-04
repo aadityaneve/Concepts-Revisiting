0be00d4e4c2201f8782953b6964e51b84101f6ff
Aditya Neve <aditya.neve@gmail.com>
2022-03-04 23:44:03 +0530

    hoisting

--- JavaScript/hoisting/index.js
+++ JavaScript/hoisting/index.js
@@ -1,8 +1,34 @@
-aGetName();
-console.log(a);
-console.log(aGetName);
+console.clear();
 
-var a = 7;
-function aGetName() {
-    console.log('Hello Aditya!');
-}
+/* EX-1 */
+/* getName();
+console.log(x);
+
+var x = 7;
+
+function getName() {
+    console.log('Namaste JavaScript');
+} */
+/* EX-1 */
+
+/* EX-2 */
+/* console.log(x2);
+getName2();
+
+var x2 = 10;
+
+var getName2 = function () {
+    console.log('Namaste JS');
+}; */
+/* EX-2 */
+
+/* EX-3 */
+console.log(x3);
+getName3();
+
+var x3 = 10;
+
+var getName3 = () => {
+    console.log('Namaste JS');
+};
+/* EX-3 */
